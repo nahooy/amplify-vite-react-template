da79b5ed9b356087777ee5404c6358841a7a3406
米山 菜帆 <naho_yoneyama@jsw.co.jp>
2025-04-17 17:11:37 +0900

    認証画面の翻訳を追加

--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,18 @@ import { I18n } from "aws-amplify/utils";
 // 認証画面の日本語翻訳辞書
 const authDict = {
   ja: {
-    'Sign In': 'サインイン',
-    'Sign in': 'Sign in',
+    'Back to Sign In': 'サインインに戻る',
     Email: 'メールアドレス',
+    'Enter your Email': 'メールアドレスを入力してください',
+    'Forgot password?': 'パスワードをお忘れの場合',
+    'Forgot your password?': 'パスワードをお忘れの場合',
     Password: 'パスワード',
+    'Reset your password': 'パスワードのリセット',
+    'Reset your Password': 'パスワードのリセット',
+    'Send code': 'リセット用コードを受信',
+    'Send Code': 'リセット用コードを受信',
+    'Sign In': 'サインイン',
+    'Sign in': 'サインイン',
   }
 }
 // 翻訳を設定
