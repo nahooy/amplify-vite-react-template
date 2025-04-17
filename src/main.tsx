d150e0fb567c47639de0b1e46dae3fc77ca88402
米山 菜帆 <naho_yoneyama@jsw.co.jp>
2025-04-17 13:29:15 +0900

    Translate part of auth screen

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,20 @@ import "./index.css";
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { I18n } from "aws-amplify/utils";
+
+// 認証画面の日本語翻訳辞書
+const authDict = {
+  ja: {
+    'Sign In': 'サインイン',
+    'Sign in': 'Sign in',
+    Email: 'メールアドレス',
+    Password: 'パスワード',
+  }
+}
+// 翻訳を設定
+I18n.putVocabularies(authDict);
+I18n.setLanguage('ja');
 
 Amplify.configure(outputs);
 
